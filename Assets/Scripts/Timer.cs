ae486913f3575fda95d840ffaebdd2ab0ac9cd38
GJeannin0 <j6.guillaume@gmail.com>
2020-01-16 10:49:55 +0100

    Added Photon & map

---
+++ Assets/Scripts/Timer.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Timer : MonoBehaviour
+{
+	[SerializeField] private float duration;
+	private float currentTime;
+
+	private bool running = false;
+	private bool ready = false;
+	private bool automatic = false;
+
+	[SerializeField] public delegate void CallResult();
+	public CallResult callResult;
+
+	void Update()
+    {
+		if (running)
+		{
+			if (!ready)
+			{
+				currentTime -= Time.deltaTime;
+				if (currentTime <= 0)
+				{
+					if (automatic)
+					{
+						callResult();
+						UseReady();
+					}
+					else
+						ready = true;
+					currentTime = duration;
+				}
+			}
+		}
+    }
+
+	public bool GetReady()
+	{
+		return ready;
+	}
+
+	public void UseReady()
+	{
+		if (ready)
+		{
+			ready = false;
+			currentTime = duration;
+		}
+	}
+
+	public float GetRemainingTime()
+	{
+		return currentTime;
+	}
+
+	public void SetDuration(float setDuration)
+	{
+		duration = setDuration;
+	}
+
+	public void Enable(bool active)
+	{
+		running = active;
+	}
+
+	public void TimerSetUp(bool startsReady, bool isAutomatic, float setDuration, CallResult result)
+	{
+		duration = setDuration;
+		automatic = isAutomatic;
+		if (!startsReady)
+		{
+			currentTime = duration;
+		}
+		else
+		{
+			currentTime = 0;
+			ready = startsReady;
+		}
+
+		callResult = result;
+	}
+}
